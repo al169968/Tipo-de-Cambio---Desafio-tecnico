c02cedbf1aebd3d1caf06f61dd18fe1bd9b48950
Roberto <al16996@alumnos.uacj.mx>
2023-10-23 12:09:49 -0600

    Agregar archivos de proyecto.

---
+++ Tipo de Cambio - Desafío técnico/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Tipo_de_Cambio___Desafío_técnico
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
